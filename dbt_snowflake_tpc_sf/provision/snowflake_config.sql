9e62c3eac9d628916b13cb53c7e97a32e783f31e
Oksana Glotova <ksu.glotova@gmail.com>
2022-10-18 21:25:58 -0400

    Set up stages, also snowflake config

---
+++ dbt_snowflake_tpc_sf/provision/snowflake_config.sql
@@ -0,0 +1,11 @@
+create or replace warehouse snow_test warehouse_size=small initially_suspended=true;
+-- create user dbt_user
+create or replace database dbt_project; 
+create or replace role transformer;
+grant role transformer to user dbt_user;
+grant operate on warehouse snow_test to role transformer;
+grant imported privileges on database snowflake_sample_data to transformer;
+grant usage on schema snowflake_sample_data.tpch_sf10 to transformer;
+grant usage on database dbt_project to transformer;
+grant create schema on database dbt_project to transformer;
+grant select on all tables in schema snowflake_sample_data.tpch_sf10 to transformer;
