1bc16b4991bf3dc822f3c1973e7b0591e96c10e2
Oksana Glotova <ksu.glotova@gmail.com>
2022-10-21 20:37:20 -0400

    Removed grants on personal schemas

--- dbt_snowflake_tpc_sf/provision/snowflake_config.sql
+++ dbt_snowflake_tpc_sf/provision/snowflake_config.sql
@@ -10,5 +10,3 @@ grant usage on schema snowflake_sample_data.tpch_sf10 to transformer;
 grant usage on database dbt_project to transformer;
 grant create schema on database dbt_project to transformer;
 grant select on all tables in schema snowflake_sample_data.tpch_sf10 to transformer;
-grant select on future views in schema dbt_project.dev_ksu to accountadmin;
-grant select on future tables in schema dbt_project.dev_ksu to accountadmin;
